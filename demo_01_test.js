1c42d9476783186eeb9247e809cc791c0f3931e6
Rares Gosman <hessyan@gmail.com>
2021-07-31 22:25:50 +0200

    latest

--- demo_01_test.js
+++ demo_01_test.js
@@ -3,19 +3,19 @@ var http = require('http');
 var url = require('url');
 var util = require('util');
 const { runInNewContext } = require('vm');
-const port = 8080;
+const listeningPort = 8080;
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(util.format('Request url is %s. Message is: %s. Date is: %s',req.url,"Hello World !", myDateTime()));        
     res.end();
     server.close();  
   
-}).listen(port);
+}).listen(listeningPort);
 
 http.get(
     {
         hostname : "localhost",
-        port: port,
+        port: listeningPort,
         path: "/",
         agent: false 
 
